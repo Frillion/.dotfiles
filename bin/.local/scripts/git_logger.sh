f0fd02796f4f04b1c21926ff35106f977f220713
Frillion <fridriksoebech@gmail.com>
2024-08-17 01:30:16 +0000

    Setup for dotfiles using the stow command

---
+++ bin/.local/scripts/git_logger.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [[ -z $1 ]]; then
+    selection=$(find $HOME -type d  | grep .*\.git/ -o  | uniq | xargs dirname | fzf)
+else
+    selection=$1
+fi
+
+if [[ -z $selection ]]; then
+    exit 0
+fi
+
+tmux neww -c $selection bash -c "git log --oneline --graph"
